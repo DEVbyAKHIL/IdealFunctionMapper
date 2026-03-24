0656e8511ce0d8929aa813938c0eccfc6408e7cf
AKHIL T S <158304347+DEVbyAKHIL@users.noreply.github.com>
2026-03-24 20:00:53 +0100

    Delete src/exceptions.py

--- src/exceptions.py
+++
@@ -1,8 +0,0 @@
-"""
-Custom exceptions for this project
-I made this file because, I wanted to handle the missing files for easy to debugging
-"""
-
-class DataLoadError(Exception):
-    """Custom exception for data loading errors."""
-    pass
